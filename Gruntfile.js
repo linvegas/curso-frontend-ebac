82b1c5dfe572039b2ebe6ee8499d42c333d9dbd2
MisterConscio <saulo.costam@gmail.com>
2023-02-06 19:08:30 -0300

    Remoção de comentários

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,47 +2,12 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     less: {
-      // development: {
-      //   files: { 'dev/styles/main.css': 'src/styles/main.less' }
-      // },
       production: {
         options: { compress: true },
         files: { 'dist/styles/main.min.css': 'src/styles/main.less' }
       }
     },
-    // watch: {
-    //   less: {
-    //     files: ['src/styles/**/*.less'],
-    //     tasks: ['less:development']
-    //   },
-    //   html: {
-    //     files: ['src/index.html'],
-    //     tasks: ['replace:dev']
-    //   }
-    // },
     replace: {
-      // dev: {
-      //   options: {
-      //     patterns: [
-      //       {
-      //         match: 'CSS',
-      //         replacement: './styles/main.css'
-      //       },
-      //       {
-      //         match: 'JS_SORT',
-      //         replacement: '../src/scripts/main.js'
-      //       }
-      //     ]
-      //   },
-      //   files: [
-      //     {
-      //       expand: true,
-      //       flatten: true,
-      //       src: ['src/index.html'],
-      //       dest: 'dev/'
-      //     }
-      //   ]
-      // },
       dist: {
         options: {
           patterns: [
@@ -66,15 +31,6 @@ module.exports = function(grunt) {
         ]
       }
     },
-    // htmlmin: {
-    //   dist: {
-    //     options: {
-    //       removeComments: true,
-    //       collapseWhitespace: true
-    //     },
-    //     files: { 'prebuild/index.html': 'src/index.html' }
-    //   }
-    // },
     clean: ['prebuild'],
     uglify: {
       target: {
@@ -84,12 +40,9 @@ module.exports = function(grunt) {
   })
 
   grunt.loadNpmTasks('grunt-contrib-less')
-  // grunt.loadNpmTasks('grunt-contrib-watch')
-  // grunt.loadNpmTasks('grunt-contrib-htmlmin')
   grunt.loadNpmTasks('grunt-contrib-clean')
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-replace')
 
-  // grunt.registerTask("default", ['watch'])
   grunt.registerTask("build", ['less:production', 'replace:dist', 'uglify', 'clean'])
 }
