7dea3bef1d3ea2585ae0acc46c58e360344f1b53
MisterConscio <saulo.costam@gmail.com>
2023-03-23 12:30:21 -0300

    Exercicio sobre typescript

---
+++ src/type.js
@@ -0,0 +1,9 @@
+"use strict";
+const multiplique = (a, b) => {
+    return a * b;
+};
+const saudacoes = (nome) => {
+    return `Olá ${nome}, como está?`;
+};
+console.log(multiplique(3, 5));
+console.log(saudacoes("João"));
---
+++ src/type.ts
@@ -0,0 +1,10 @@
+const multiplique = (a: number, b: number): number => {
+  return a * b
+}
+
+const saudacoes = (nome: string): string => {
+  return `Olá ${nome}, como está?`
+}
+
+console.log(multiplique(3, 5))
+console.log(saudacoes("João"))
